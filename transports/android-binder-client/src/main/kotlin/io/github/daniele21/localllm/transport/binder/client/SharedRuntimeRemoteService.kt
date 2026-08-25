92f802c024e0464b08c9ba58edbc34ba0dc1c47e
Daniele21 <daniele21@users.noreply.github.com>
2026-08-25 23:12:47 +0200

    style(binder): apply ktlint to v2 remote service

--- transports/android-binder-client/src/main/kotlin/io/github/daniele21/localllm/transport/binder/client/SharedRuntimeRemoteService.kt
+++ transports/android-binder-client/src/main/kotlin/io/github/daniele21/localllm/transport/binder/client/SharedRuntimeRemoteService.kt
@@ -36,12 +36,8 @@ internal interface ConsumerSharedRuntimeRemoteService {
     fun generate(request: ConsumerRequestParcel, callback: (ConsumerGenerationEventParcel) -> Unit)
 
     @Throws(RemoteException::class)
-    fun generateV2(
-        request: ConsumerGenerationRequestV2Parcel,
-        callback: (ConsumerGenerationEventParcel) -> Unit,
-    ) {
+    fun generateV2(request: ConsumerGenerationRequestV2Parcel, callback: (ConsumerGenerationEventParcel) -> Unit): Unit =
         throw RemoteException("Consumer generation v2 is unavailable")
-    }
 
     @Throws(RemoteException::class)
     fun cancel(request: CancelRequestParcel)
